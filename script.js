d9e2e55c4c7b4b926924140c5c01f873183b036e
Idriz Altes <99037664+LetRookie@users.noreply.github.com>
2023-07-25 10:18:04 +0300

    new update

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-window.addEventListener("scroll", function () {
-    let header = document.querySelector("header");
-    let logo = document.getElementById("logo");
-    header.classList.toggle("sticky", window.scrollY > 0);
-    logo.classList.toggle("active", window.scrollY > 0);
-});
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
